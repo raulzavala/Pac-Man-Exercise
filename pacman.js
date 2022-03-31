d0104c0297b9253f4732a8d0cc0a834b0fb2a3bf
Raul Zavala <iscraulzavala@gmail.com>
2022-03-30 18:08:29 -0700

    Pacman Animation

---
+++ pacman.js
@@ -0,0 +1,45 @@
+// this variable defines what direction should PacMan go into:
+// 0 = left to right
+// 1 = right to left (reverse)
+var direction = 0;
+// pos is the PacMan image position variable- it is set to 0 initially
+var pos = 0;
+//pageWidth is the width of the webpage. This is later used to calculate when Pac-Man needs to turn around.
+let pageWidth = window.innerWidth;
+//This array contains all the PacMan movement images
+const pacArray = [
+  ["./images/PacMan1.png", "./images/PacMan2.png"],
+  ["./images/PacMan3.png", "./images/PacMan4.png"],
+];
+// This variable helps determine which PacMan image should be displayed. It flips between values 0 and 1
+var focus = 0;
+let img = document.getElementById("PacMan");
+let imgWidth = img.width;
+// This function is called on mouse click. Every time it is called, it updates the PacMan image, position and direction on the screen.
+function Run() {
+  focus = (focus + 1) % 2;
+  direction = checkPageBounds(direction, imgWidth, pos, pageWidth);
+  img.src = pacArray[direction][focus];
+  if (direction) {
+    pos -= 20;
+    img.style.left = pos + "px";
+  } else {
+    pos += 20;
+    img.style.left = pos + "px";
+  }
+}
+
+setInterval(function () {
+  checkPageBounds(direction, imgWidth, pos, pageWidth);
+  Run();
+}, 200);
+
+// This function determines the direction of PacMan based on screen edge detection.
+function checkPageBounds(direction, imgWidth, pos, pageWidth) {
+  if (direction == 1 && pos < 0) {
+    direction = 0;
+  } else if (direction == 0 && pos + imgWidth > pageWidth) {
+    direction = 1;
+  }
+  return direction;
+}
